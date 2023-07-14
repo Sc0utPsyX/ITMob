9c754845969ce2c299dfd1a2b2ad3021d9d0fecb
godilla <godilla@yandex.ru>
2023-07-14 14:45:27 +0300

    https://yougile.com/team/7fc5a57b9457/#chat:84230ad85a40

--- target/classes/static/events.js
+++ target/classes/static/events.js
@@ -2,23 +2,34 @@
 angular.module('app', []).controller('eventsController', function ($scope, $http) {
 
     $scope.loadEvents = function () {
-        $http.get('http://localhost:8193/event/api/v1/events').then(function (response) {
+        $http.get('http://localhost:8195/event/api/v1/events').then(function (response) {
             $scope.eventsList = response.data;
-            console.log(response.data);
+            //console.log(response.data);
         });
     }
 
     $scope.showEventInfo = function (eventId) {
-        $http.get('http://localhost:8193/event/api/v1/events/' + eventId).then(function (response) {
+        $http.get('http://localhost:8195/event/api/v1/events/' + eventId).then(function (response) {
             alert(response.data.description);
+            console.log('http://localhost:8195/event/api/v1/events/' + eventId);
         });
     }
 
     $scope.deleteEventById = function (eventId) {
-        $http.delete('http://localhost:8193/event/api/v1/events/' + eventId).then(function (response) {
+        $http.delete('http://localhost:8195/event/api/v1/events/' + eventId).then(function (response) {
             $scope.loadEvents();
         });
     }
+
+    $scope.isUserLoggedIn = function () {
+        //TODO $localStorage.winterMarketUser
+        return true;
+    };
+
+    $scope.isUserAdmin = function () {
+        //TODO как будет модерация, можно будет события удалять
+        return false;
+    };
 
     $scope.loadEvents();
 
