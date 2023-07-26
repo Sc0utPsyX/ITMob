ff3e3afe40409f3356154ec2423d5dd169483e0d
godilla <godilla@yandex.ru>
2023-07-26 11:49:09 +0300

    https://ru.yougile.com/team/7fc5a57b9457/#chat:735a22d56d96

--- front-service/src/main/java/ru/gb/ITMob/social/front/controllers/MenuListController.java
+++ front-service/src/main/java/ru/gb/ITMob/social/front/controllers/MenuListController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.gb.ITMob.social.front.converters.MenuListConverter;
 import ru.gb.ITMob.social.front.dtos.MenuListDto;
 import ru.gb.ITMob.social.front.entities.MenuList;
 import ru.gb.ITMob.social.front.exceptions.ResourceNotFoundException;
@@ -20,15 +21,17 @@ public class MenuListController {
 
     private final MenuListService menuListService;
 
+    private final MenuListConverter menuListConverter;
+
     @GetMapping
     public List<MenuListDto> findAllMenuList() {
-        return menuListService.findAll().stream().map(p -> new MenuListDto(p.getId(), p.getName(), p.getPhoto_link(), p.getHref())).collect(Collectors.toList());
+        return menuListService.findAll().stream().map(menuListConverter::entityToDto).collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
     public MenuListDto findMenuListById(@PathVariable long id) {
         MenuList p = menuListService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Раздел меню не найден, id: " + id));
-        return new MenuListDto(p.getId(), p.getName(), p.getPhoto_link(), p.getHref());
+        return menuListConverter.entityToDto(p);
     }
 
 }
