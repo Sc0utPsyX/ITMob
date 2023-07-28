072077c2e6d9b8fd67f0a92d07f5ef5c4cf47feb
code-Production <code-Production@github.com>
2023-07-28 22:29:29 +0300

    REGISTRY-SERVICE + GATEWAY-SERVICE + USER-SERVICE fix

---
+++ registry-service/src/main/java/ru/gb/itmob/social/registryservice/RegistryServiceApplication.java
@@ -0,0 +1,19 @@
+package ru.gb.itmob.social.registryservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import ru.gb.itmob.social.registryservice.examples.GetUrlExample;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryServiceApplication {
+    @Autowired
+    private GetUrlExample discoveryService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryServiceApplication.class, args);
+    }
+
+}
