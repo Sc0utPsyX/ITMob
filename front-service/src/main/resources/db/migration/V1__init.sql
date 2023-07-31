6a8569a25286b49e2eeb5aa0cf7ab5b8b0e73d65
godilla <godilla@yandex.ru>
2023-08-01 01:30:53 +0300

    https://yougile.com/team/7fc5a57b9457/#chat:735a22d56d96

--- front-service/src/main/resources/db/migration/V1__init.sql
+++ front-service/src/main/resources/db/migration/V1__init.sql
@@ -3,5 +3,5 @@ insert into menu_list (name, photo_link, href) values
 ('Главная страница', 'images_menu/558900.PNG', '#!/'),
 ('Поиск события', 'images_menu/459947.PNG', '#!/events'),
 ('Добавить событие', 'images_menu/117885.PNG', '#!/events-card'),
-('Сообщения', 'images_menu/456501.PNG', ''),
-('Личный кабинет', 'images_menu/12126177.PNG', '');
+('Сообщения', 'images_menu/456501.PNG', '#!/chat'),
+('Личный кабинет', 'images_menu/12126177.PNG', '#!/profile');
---
+++ front-service/src/main/resources/static/profile/profile.js
@@ -0,0 +1,4 @@
+angular.module('socialnetwork').controller('profileController', function ($scope, $http) {
+
+
+});
